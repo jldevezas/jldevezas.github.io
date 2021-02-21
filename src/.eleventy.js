8c002e8b54ad223bb00e4d978c1756b3ee77985e
José Devezas <joseluisdevezas@gmail.com>
2021-02-21 20:28:41 +0000

    Experimenting with the layout, while depressing.

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
 	eleventyConfig.setTemplateFormats(['md', 'liquid', 'css']);
 
-  eleventyConfig.addPassthroughCopy('css/*.css')
+  eleventyConfig.addPassthroughCopy('css')
 
 	eleventyConfig.addPassthroughCopy({
 		'node_modules/spectre.css/dist/spectre.min.css': 'css/spectre.min.css'
